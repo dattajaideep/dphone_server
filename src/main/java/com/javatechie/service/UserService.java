4d6b635298f4d0b6b5bc1a81007300c4dc270980
Appani Kaushik <appanikaushik@L-YN4LWQHP4F.local>
2023-02-18 13:28:51 +0530

    referal code

--- src/main/java/com/javatechie/service/UserService.java
+++ src/main/java/com/javatechie/service/UserService.java
@@ -3,6 +3,10 @@ package com.javatechie.service;
 import com.javatechie.beans.UserBean;
 import com.javatechie.entity.UserEntity;
 import com.javatechie.repository.UserInfoRepository;
+
+import java.security.SecureRandom;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,11 +18,25 @@ public class UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    public static String code(int codeLength) {
+        char[] chars = "abcdefghijklmnopqrstuvwxyz1234567890".toCharArray();
+        StringBuilder sb = new StringBuilder();
+        Random random = new SecureRandom();
+        for (int i = 0; i < codeLength; i++) {
+            char c = chars[random.nextInt(chars.length)];
+            sb.append(c);
+        }
+        String output = sb.toString();
+        System.out.println(output);
+        return output;
+    }
+
     public String addUser(UserEntity userInfo) {
         try {
             System.out.println(userInfo);
             userInfo.setPoints(0);
             userInfo.setRoles("USER");
+            userInfo.setReferalCode(code(8));
             userInfo.setPassword(passwordEncoder.encode(userInfo.getPassword()));
             repository.save(userInfo);
             return "user added to system ";
